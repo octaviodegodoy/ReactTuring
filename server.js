c59b320988a262f8ef80615e36323449e33e95ab
octaviodegodoy <octaviodegodoy@gmail.com>
2019-03-07 23:40:39 -0300

    A star is born

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.listen(3000, function () { 
+    console.log('Express server is on port 3000');
+});
